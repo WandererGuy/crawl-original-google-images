1d01c7545d587db90f59fb39f83ae62d7f9b1212
Duc Manh <manhtech264@gmail.com>
2025-02-16 16:52:20 +0700

    Update my_crawl_url.py

--- my_crawl_url.py
+++ my_crawl_url.py
@@ -13,11 +13,7 @@ from tqdm import tqdm
 import random
 import shutil
 import urllib
-
-
-
 import io
-
 from PIL import Image  # https://pillow.readthedocs.io/en/4.3.x/
 import requests  # http://docs.python-requests.org/en/master/
 
@@ -35,10 +31,6 @@ def download_image(url, image_file_path):
     print('Image downloaded from url: {} and saved to: {}.'.format(url, image_file_path))
 
 
-service = Service(executable_path='./chromedriver.exe')
-options = webdriver.ChromeOptions()
-DRIVER= webdriver.Chrome(service=service, options=options)
-
 def find_max_int(filepath):
     ls = []
     for filename in os.listdir(filepath):
@@ -46,9 +38,16 @@ def find_max_int(filepath):
     return max(ls)
 
 
+service = Service(executable_path='./chromedriver.exe')
+options = webdriver.ChromeOptions()
+DRIVER= webdriver.Chrome(service=service, options=options)
+
 
 # kw_dict = {"fishing boat asia": "fishing_boat_asia"}
 kw_search = "tau van tai trung quoc"
+
+
+
 kw_search_value = "_".join(kw_search.split(' '))
 kw_dict = {kw_search: kw_search_value}
 
@@ -64,25 +63,12 @@ if __name__ == "__main__":
     os.makedirs(f"images/{folder_keyword}", exist_ok=True)
 
     fail_src_url = "data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-
-    # URL = "https://www.google.com/search?sca_esv=719323f8b4394607&q=tau+danh+ca&udm=2&fbs=ABzOT_AfCikcO6SgGMxZXxAG9tmS8rx53CbgOCSVg3O9Xo5xAK_RXi3VFy8QcDJV9F46BNX0kZcBdv2qiG8279sznoGvwXYVfqXi5AxPvDXkJ-MkwThOeWf1fl0-vP_stZw2-wiFo2_fcg96rRhBCMfKN9fd7eIVFeeEix-moWmSapYEMEksBbaReod_JR2StsZ_J6rZI-OtKz6sLewu_ahkl0bzIipJiM2sgx0UvR4EsxPbUSRk_Cs&sa=X&ved=2ahUKEwi01vqv7cKLAxUirlYBHR0DM84QtKgLegQIGRAB&biw=1920&bih=945"
-    # DRIVER.get(URL) 
-
-    # Open Google
-
-    # Find the input area (Google search bar)
-
-
-    # Clear any pre-filled text (if any)
     input_area.clear()
-
-    # Type the text you want to search
     text_to_search = search_keyword
     input_area.send_keys(text_to_search)
 
     # Simulate pressing the 'Enter' key to start the search
     input_area.send_keys(Keys.RETURN)
-    # max_num = find_max_int(filepath = f"images/{folder_keyword}")
     time.sleep(3)
     for n in range(1, 150):
         if n % 10 == 0: 
